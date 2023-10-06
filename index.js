4cfd65b8a89024110fbbff240ddb3e001f639da0
user.name: "thelastmedici <user.email: "christianopeyemijoshua@gmail.com>
2023-10-06 19:05:28 +0100

    added js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
-const detailsForm = document.getElementById('details')
+const consentForm= document.getElementById('consent-form')
+const modalText = document.getElementById('modal-text')
  
 setTimeout(function(){
     modal.style.display = 'inline'
@@ -10,8 +11,15 @@ setTimeout(function(){
 modalCloseBtn.addEventListener('click', function(){
     modal.style.display = 'none'
 })
-detailsForm.addEventListener('submit', function(e){
+consentForm.addEventListener('submit', function(e){
     e.preventDefault()
-
+    modalText.innerHTML = `
+        <div class="modal-inner-loading">
+        <img src="images/loading.svg" class="loading">
+        <p id="uploadText">
+            Uploading your data to the dark web...
+        </p>
+    </div>
+`
 })
 
