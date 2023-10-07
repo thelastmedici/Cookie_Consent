923cb648c1942421a5ee0eed13c0afa901c27d88
user.name: "thelastmedici <user.email: "christianopeyemijoshua@gmail.com>
2023-10-07 13:13:59 +0100

    added functionalities

--- index.js
+++ index.js
@@ -13,13 +13,36 @@ modalCloseBtn.addEventListener('click', function(){
 })
 consentForm.addEventListener('submit', function(e){
     e.preventDefault()
-    modalText.innerHTML = `
+    const consentFormData = new FormData(consentForm)
+    const fullname = consentFormData.get('fullname')
+    console.log(fullname)
+    modalText.innerHTML = 
+    `
         <div class="modal-inner-loading">
         <img src="images/loading.svg" class="loading">
-        <p id="uploadText">
-            Uploading your data to the dark web...
+        <p id="upload-text">
+            Uploading your data to the dark web})...
         </p>
     </div>
 `
-})
+setTimeout(function(){
+document.getElementById('upload-text').textContent= 
+    `
+        Making sure you're not a cop...
+    `
+}, 1500)
 
+
+
+setTimeout(function(){
+    document.getElementById('modal-inner').innerHTML = `
+    <h2>Thanks <span class="modal-display-name">${fullname}</span>, you PIRATEEEEEEE! </h2>
+    <p>Now we have initiated you to our dark world.</p>
+    <div class="idiot-gif">
+        <img src="images/pirate.gif">
+    </div>
+    ` 
+},3000)
+
+
+})
